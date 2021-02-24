eec8b7caccaacadae7e428b192bc97b8a69349de
呐喊 <1250260404@qq.com>
2021-02-24 13:38:04 +0800

    项目初始化-调整初始目录结构

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,26 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-trailing-spaces': 'off',
+    'no-tabs': 'off',
+		'no-mixed-spaces-and-tabs': 'off',
+		'indent': 'off',
+    'space-before-function-paren': 'off',
+    'keyword-spacing': 'off',
+    'spaced-comment': 'off',
+    'key-spacing': 'off',
+    'comma-dangle': 'off',
+    'object-curly-spacing': 'off',
+    'comma-spacing': 'off',
+    'space-before-blocks': 'off',
+    'padded-blocks': 'off',
+    'quotes': 'off',
+    'semi': 'off',
+    'quote-props': 'off',
+    'no-multiple-empty-lines': 'off',
+    'vue/no-unused-vars': 'off',
+    'no-useless-return': 'off',
+    'eol-last':'off'
   }
 }
